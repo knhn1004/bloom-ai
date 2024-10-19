c080764e5c24232a121d7570009a1ebe0fa7625b
enkhbold470 <enkhbold470@gmail.com>
2024-10-18 21:56:12 -0700

    Add light sensor and soil moisture sensor code

---
+++ hardware_src/soilMoisture_test/soilMoisture_test.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+Serial.print("On UART...");
+
+}
+
+void loop() {
+  int value = analogRead(A1);
+  // put your main code here, to run repeatedly:
+Serial.println(value);
+delay(50);
+}
