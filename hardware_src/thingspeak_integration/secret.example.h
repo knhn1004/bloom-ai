b083cfba32ffc626411b452db5f0517c63264cd3
enkhbold470 <enkhbold470@gmail.com>
2024-10-18 19:05:06 -0700

    Add .gitignore file and update AnalogInput.txt
    - Added .gitignore file to ignore secret.h and .env files in the thingspeak_integration directory.
    - Updated AnalogInput.txt with instructions on how to use a potentiometer to control the blinking of an LED.

---
+++ hardware_src/thingspeak_integration/secret.example.h
@@ -0,0 +1,8 @@
+// secret.h
+// WiFi credentials
+#define SECRET_SSID "xxxxx"
+#define SECRET_PASS "xxxxx"
+
+// ThingSpeak credentials
+#define THINGSPEAK_CHANNEL xxxx // Example: 123456
+#define THINGSPEAK_API_KEY "xxxx"
